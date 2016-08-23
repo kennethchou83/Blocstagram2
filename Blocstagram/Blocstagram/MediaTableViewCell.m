7f2511eb9e2ec70c71b1c216459ec02cafffd559
Kenneth Chou <kennethchou83@yahoo.com>
2016-08-22 23:06:39 -0700

    checkpoint_30 assignment completed2

--- Blocstagram/Blocstagram/MediaTableViewCell.m
+++ Blocstagram/Blocstagram/MediaTableViewCell.m
@@ -182,7 +182,7 @@ static NSParagraphStyle *paragraphStyle;
     
     self.usernameAndCaptionLabelHeightConstraint.constant = usernameLabelSize.height + 20;
     self.commentLabelHeightConstraint.constant = commentLabelSize.height + 20;
-    self.imageHeightConstraint.constant = 100 * 100;
+    self.imageHeightConstraint.constant = 100;
 //    self.imageHeightConstraint.constant = self.mediaItem.image.size.height / self.mediaItem.image.size.width * CGRectGetWidth(self.contentView.bounds);
     
     
